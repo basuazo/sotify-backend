5db4cdac46cd0fd5d1645e5becb5aa8a0e9dd25a
Boris Suazo <borisanibalsuazo@gmail.com>
2024-07-11 20:55:48 -0400

    endpoints artistas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Artista } from './artistas';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  artistas: Artista[] = [];
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('artistas')
+  obtenerArtistas(): Artista[] {
+    // this.artistas.push(
+    //   new Artista(1, 'juan', 'bio', 'banda', 'chilena', ['rock'], 1),
+    // );
+    return this.artistas;
+  }
+
+  @Post('artistas')
+  nuevoArtista(@Body() artista: Artista): Artista {
+    this.artistas.push(artista);
+    return null;
   }
 }
